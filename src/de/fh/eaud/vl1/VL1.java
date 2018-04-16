89dc257abdcc65e291528c45067bd68fa2365c0f
DerStudent <strolsu@googlemail.com>
2018-04-16 13:35:47 +0100

    deleted sysout

--- src/de/fh/eaud/vl1/VL1.java
+++ src/de/fh/eaud/vl1/VL1.java
@@ -14,14 +14,14 @@ public class VL1 {
 
 	public void durchlaufen(int max) {
 		for (int i = 1; i <= max; i++) {
-			// System.out.println(i);
+			System.out.println(i);
 		}
 	}
 
 	public void doppelTupel(int max) {
 		for (int i = 1; i <= max; i++) {
 			for (int j = 1; j <= max; j++) {
-				// System.out.println("(" + i + "," + j + ")");
+				System.out.println("(" + i + "," + j + ")");
 			}
 		}
 	}
