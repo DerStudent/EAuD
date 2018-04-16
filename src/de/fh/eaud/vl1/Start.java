715bef12a2a90262d8d223690052c2f3c4dbc59b
DerStudent <strolsu@googlemail.com>
2018-04-16 12:51:33 +0100

    potenzmenge added

--- src/de/fh/eaud/vl1/Start.java
+++ src/de/fh/eaud/vl1/Start.java
@@ -4,7 +4,7 @@ import de.fh.eaud.stopwatch.StopWatch;
 
 public class Start {
 
-	private static final int MAX = 1000000;
+	private static final int MAX = 10;
 
 	public static void main(String[] args) {
 		StopWatch st = new StopWatch();
@@ -13,7 +13,8 @@ public class Start {
 		st.start();
 		
 		//test.durchlaufen(MAX);
-		test.doppelTupel(MAX);
+		//test.doppelTupel(MAX);
+		test.teilmengen(MAX);
 		
 		st.stop();
 		System.out.println("Die benötigte Zeit für " + MAX + " Elemente betrug: "+ st.getTimeNano() + " nano sec.");
--- src/de/fh/eaud/vl1/VL1.java
+++ src/de/fh/eaud/vl1/VL1.java
@@ -1,5 +1,11 @@
 package de.fh.eaud.vl1;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
 public class VL1 {
 
 	public VL1() {
@@ -15,12 +21,40 @@ public class VL1 {
 	public void doppelTupel(int max) {
 		for (int i = 1; i <= max; i++) {
 			for (int j = 1; j <= max; j++) {
-				//System.out.println("(" + i + "," + j + ")");
+				// System.out.println("(" + i + "," + j + ")");
 			}
 		}
 	}
 
 	public void teilmengen(int max) {
+		Set<Integer> mySet = new HashSet<Integer>();
 
+		for (int i = 1; i <= max; i++) {
+			mySet.add(i);
+			//System.out.println("Added: " + i);
+		}
+		for (Set<Integer> s : powerSet(mySet)) {
+			//System.out.println(s);
+		}
 	}
+
+	public static <T> Set<Set<T>> powerSet(Set<T> originalSet) {
+		Set<Set<T>> sets = new HashSet<Set<T>>();
+		if (originalSet.isEmpty()) {
+			sets.add(new HashSet<T>());
+			return sets;
+		}
+		List<T> list = new ArrayList<T>(originalSet);
+		T head = list.get(0);
+		Set<T> rest = new HashSet<T>(list.subList(1, list.size()));
+		for (Set<T> set : powerSet(rest)) {
+			Set<T> newSet = new HashSet<T>();
+			newSet.add(head);
+			newSet.addAll(set);
+			sets.add(newSet);
+			sets.add(set);
+		}
+		return sets;
+	}
+
 }
